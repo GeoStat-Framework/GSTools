7b30c9486b55f57b180e12dcf7069bdc2310a403
LSchueler <lennart.schueler@ufz.de>
2018-11-30 10:31:15 +0100

    unittest for RNG now updated

--- tests/test_rng.py
+++ tests/test_rng.py
@@ -8,104 +8,106 @@ import unittest
 import numpy as np
 from scipy.stats import (skew, kurtosis)
 from scipy.stats import normaltest
-from gstools.field import (RNG, RandMeth)
+from gstools import (Gaussian, TPLStable)
+from gstools.random.rng import RNG
 
 
 class TestRNG(unittest.TestCase):
     def setUp(self):
-        self.rngs = [RNG(1, 19031977), RNG(2, 19031977), RNG(3, 19031977)]
+        self.seed = 19031977
+        self.rng = RNG(self.seed)
         self.many_modes = 1000000
         self.few_modes = 100
-        self.len_scale = 20.
 
-    def test_rng_consistency_1d(self):
-        rng = RNG(1, 21021997)
-        l = 2.5
-        Z_refs = [-0.04100185,
-                   3.33332132,
-                   0.22111093,
-                  -1.30856723,
-                   2.40095964
+    def test_rng_normal_consistency(self):
+        rng = RNG(21021997)
+        z1_refs = [
+                    -1.93013270,
+                     0.46330478,
                  ]
-        k_refs = [-0.107329380,
-                   0.407318334,
-                   1.067490292,
-                   1.256078044,
-                   0.100332117
+        z2_refs = [
+                    -0.25536086,
+                     0.98298696,
                  ]
 
-        #explicit for better debugging, so the line is found immediately
-        Z, k = rng('gau', l, mode_no=1)
-        self.assertAlmostEqual(Z[0, 0], Z_refs[0])
-        self.assertAlmostEqual(k[0, 0], k_refs[0])
-        Z, k = rng('gau', l, mode_no=1)
-        self.assertAlmostEqual(Z[0, 0], Z_refs[1])
-        self.assertAlmostEqual(k[0, 0], k_refs[1])
+        z1 = self.rng.random.normal(size=self.few_modes)
+        z2= self.rng.random.normal(size=self.few_modes)
+        self.assertAlmostEqual(z1[0], z1_refs[0])
+        self.assertAlmostEqual(z1[1], z1_refs[1])
+        self.assertAlmostEqual(z2[0], z2_refs[0])
+        self.assertAlmostEqual(z2[1], z2_refs[1])
+        self.rng.seed = self.seed
+        z1 = self.rng.random.normal(size=self.few_modes)
+        z2 = self.rng.random.normal(size=self.few_modes)
+        self.assertAlmostEqual(z1[0], z1_refs[0])
+        self.assertAlmostEqual(z1[1], z1_refs[1])
+        self.assertAlmostEqual(z2[0], z2_refs[0])
+        self.assertAlmostEqual(z2[1], z2_refs[1])
 
-    def test_rng_consistency_2d(self):
-        rng = RNG(2, 21021997)
-        l = 2.5
-        Z_refs = [-0.04100185,
-                   1.01829584,
-                  -1.30856723,
-                   2.17740051,
-                  -0.32662374
-                 ]
-        k_refs = [-0.107329380,
-                   0.088444371,
-                  -4.047265921,
-                   0.873734481,
-                  -0.002127327
-                 ]
+    def test_sample_sphere_1d(self):
+        dim = 1
+        sphere_coord = self.rng.sample_sphere(dim, self.few_modes)
+        self.assertEqual(sphere_coord.shape, (dim, self.few_modes))
+        sphere_coord = self.rng.sample_sphere(dim, self.many_modes)
+        self.assertAlmostEqual(np.mean(sphere_coord), 0.0, places=3)
 
-        #explicit for better debugging, so the line is found immediately
-        Z, k = rng('gau', l, mode_no=1)
-        self.assertAlmostEqual(Z[0, 0], Z_refs[0])
-        self.assertAlmostEqual(k[0, 0], k_refs[0])
-        Z, k = rng('gau', l, mode_no=1)
-        self.assertAlmostEqual(Z[0, 0], Z_refs[1])
-        self.assertAlmostEqual(k[0, 0], k_refs[1])
+    def test_sample_sphere_2d(self):
+        dim = 2
+        sphere_coord = self.rng.sample_sphere(dim, self.few_modes)
+        np.testing.assert_allclose(np.ones(self.few_modes),
+                sphere_coord[0,:]**2 + sphere_coord[1,:]**2)
+        sphere_coord = self.rng.sample_sphere(dim, self.many_modes)
+        self.assertAlmostEqual(np.mean(sphere_coord), 0.0, places=3)
 
-    def test_rng_consistency_3d(self):
-        rng = RNG(3, 21021997)
-        l = 2.5
-        Z_refs = [-0.04100185,
-                   0.22111093,
-                   2.40095964,
-                  -0.32578687,
-                  -1.96961056
-                 ]
-        k_refs = [-0.107329380,
-                   0.092119696,
-                  -0.216183123,
-                   0.538085014,
-                  -0.723404118
-                 ]
+    def test_sample_sphere_3d(self):
+        dim = 3
+        # TODO any trigonometric identity similar to sphere_2d?
+        sphere_coord = self.rng.sample_sphere(dim, self.few_modes)
+        self.assertEqual(sphere_coord.shape, (dim, self.few_modes))
+        sphere_coord = self.rng.sample_sphere(dim, self.many_modes)
+        self.assertAlmostEqual(np.mean(sphere_coord), 0.0, places=3)
 
-        #explicit for better debugging, so the line is found immediately
-        Z, k = rng('gau', l, mode_no=1)
-        self.assertAlmostEqual(Z[0, 0], Z_refs[0])
-        self.assertAlmostEqual(k[0, 0], k_refs[0])
-        Z, k = rng('gau', l, mode_no=1)
-        self.assertAlmostEqual(Z[0, 0], Z_refs[1])
-        self.assertAlmostEqual(k[0, 0], k_refs[1])
 
-    def test_call(self):
-        for d in range(len(self.rngs)):
-            Z, k = self.rngs[d]('gau', self.len_scale, self.many_modes)
-            self.assertEqual(Z.shape, (2, self.many_modes))
-            self.assertAlmostEqual(np.mean(Z), 0., places=2)
-            self.assertAlmostEqual(np.std(Z), 1., places=2)
+    def test_sample_dist(self):
+        model = Gaussian(dim=1, var=3.5, len_scale=8.)
+        pdf, cdf, ppf = model.dist_func
+        rad = self.rng.sample_dist(
+            size=self.few_modes, pdf=pdf, cdf=cdf, ppf=ppf, a=0)
+        self.assertEqual(rad.shape[0], self.few_modes)
 
-    def test_gau(self):
-        for d in range(len(self.rngs)):
-            Z, k = self.rngs[d]('gau', self.len_scale, self.many_modes)
-            self.assertEqual(k.shape, (d+1, self.many_modes))
-            self.assertAlmostEqual(np.mean(k), 0., places=2)
-            self.assertAlmostEqual(np.std(k), 1/self.len_scale, places=2)
-            self.assertAlmostEqual(skew(k[0, :]), 0., places=2)
-            self.assertAlmostEqual(kurtosis(k[0, :]), 0., places=1)
-            self.assertLess(normaltest(k[0, :])[1], 0.05)
+        model = Gaussian(dim=2, var=3.5, len_scale=8.)
+        pdf, cdf, ppf = model.dist_func
+        rad = self.rng.sample_dist(
+            size=self.few_modes, pdf=pdf, cdf=cdf, ppf=ppf, a=0)
+        self.assertEqual(rad.shape[0], self.few_modes)
+
+        model = Gaussian(dim=3, var=3.5, len_scale=8.)
+        pdf, cdf, ppf = model.dist_func
+        rad = self.rng.sample_dist(
+            size=self.few_modes, pdf=pdf, cdf=cdf, ppf=ppf, a=0)
+        self.assertEqual(rad.shape[0], self.few_modes)
+
+        #model = Gaussian(dim=2, var=3.5, len_scale=8.)
+        #pdf, cdf, ppf = model.dist_func
+        #rad = self.rng.sample_dist(
+        #    size=self.many_modes, pdf=pdf, cdf=cdf, ppf=ppf, a=0)
+        #import matplotlib.pyplot as pt
+        #pt.hist(rad, bins=30)
+        #print(rad)
+        #pt.show()
+
+        # TODO test with different models
+
+    # TODO rework this
+    #def test_gau(self):
+    #    for d in range(len(self.rngs)):
+    #        Z, k = self.rngs[d]('gau', self.len_scale, self.many_modes)
+    #        self.assertEqual(k.shape, (d+1, self.many_modes))
+    #        self.assertAlmostEqual(np.mean(k), 0., places=2)
+    #        self.assertAlmostEqual(np.std(k), 1/self.len_scale, places=2)
+    #        self.assertAlmostEqual(skew(k[0, :]), 0., places=2)
+    #        self.assertAlmostEqual(kurtosis(k[0, :]), 0., places=1)
+    #        self.assertLess(normaltest(k[0, :])[1], 0.05)
 
 
 if __name__ == '__main__':
