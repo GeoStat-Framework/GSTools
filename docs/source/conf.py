32ac167550ff4a05fdaedf6cd11ba330177f6e27
LSchueler <mostem@posteo.de>
2019-01-14 16:40:40 +0100

    Added a small note to sphinx conf informing about requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+# NOTE:
+# pip install sphinx_rtd_theme
+# is needed in order to build the documentation
 import os
 import sys
 
