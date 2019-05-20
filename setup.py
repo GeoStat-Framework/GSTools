44a71f59bced1e2deeca0ac78eb9813b123d437a
MuellerSeb <mueller.seb@posteo.de>
2019-05-20 19:41:38 +0200

    cleanup setup.py; bugfix for python2

--- setup.py
+++ setup.py
@@ -5,18 +5,12 @@ import sys
 import os
 import codecs
 import re
-import logging
 import tempfile
 import glob
 import subprocess
+import shutil
 
-from distutils.errors import (
-    CompileError,
-    LinkError,
-    CCompilerError,
-    DistutilsExecError,
-    DistutilsPlatformError,
-)
+from distutils.errors import CompileError, LinkError
 from distutils.ccompiler import new_compiler
 from distutils.sysconfig import customize_compiler
 
@@ -27,26 +21,17 @@ from Cython.Distutils.extension import Extension
 
 import numpy
 
-logging.basicConfig()
-log = logging.getLogger(__file__)
-
-ext_errors = (
-    CCompilerError,
-    DistutilsExecError,
-    DistutilsPlatformError,
-    IOError,
-)
-
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 # version finder ##############################################################
 
+
 def read(*parts):
     """read file data"""
-    with codecs.open(os.path.join(HERE, *parts), "r") as fp:
-        return fp.read()
+    with codecs.open(os.path.join(HERE, *parts), "r") as file_in:
+        return file_in.read()
 
 
 def find_version(*file_paths):
@@ -65,6 +50,19 @@ def find_version(*file_paths):
 # can be found at:
 # https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/_build_utils/openmp_helpers.py
 
+# TemporaryDirectory not avialable in python2
+class _TemporaryDirectory(object):
+    def __enter__(self):
+        self.dir_name = tempfile.mkdtemp()
+        return self.dir_name
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        shutil.rmtree(self.dir_name)
+
+
+TemporaryDirectory = getattr(
+    tempfile, "TemporaryDirectory", _TemporaryDirectory
+)
 
 CCODE = """
 #include <omp.h>
@@ -78,18 +76,19 @@ return 0;
 
 
 def get_openmp_flag(compiler):
-    if hasattr(compiler, 'compiler'):
+    """get the compiler dependent openmp flag"""
+    if hasattr(compiler, "compiler"):
         compiler = compiler.compiler[0]
     else:
         compiler = compiler.__class__.__name__
 
-    if sys.platform == "win32" and ('icc' in compiler or 'icl' in compiler):
-        return ['/Qopenmp']
-    elif sys.platform == "win32":
-        return ['/openmp']
-    elif sys.platform == "darwin" and ('icc' in compiler or 'icl' in compiler):
-        return ['-openmp']
-    elif sys.platform == "darwin" and 'openmp' in os.getenv('CPPFLAGS', ''):
+    if sys.platform == "win32" and ("icc" in compiler or "icl" in compiler):
+        return ["/Qopenmp"]
+    if sys.platform == "win32":
+        return ["/openmp"]
+    if sys.platform == "darwin" and ("icc" in compiler or "icl" in compiler):
+        return ["-openmp"]
+    if sys.platform == "darwin" and "openmp" in os.getenv("CPPFLAGS", ""):
         # -fopenmp can't be passed as compile flag when using Apple-clang.
         # OpenMP support has to be enabled during preprocessing.
         #
@@ -103,76 +102,78 @@ def get_openmp_flag(compiler):
         # export DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib
         return []
     # Default flag for GCC and clang:
-    return ['-fopenmp']
+    return ["-fopenmp"]
 
 
 def check_openmp_support():
     """Check whether OpenMP test code can be compiled and run"""
     ccompiler = new_compiler()
     customize_compiler(ccompiler)
-    start_dir = os.path.abspath('.')
 
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    with TemporaryDirectory() as tmp_dir:
         try:
             os.chdir(tmp_dir)
             # Write test program
-            with open('test_openmp.c', 'w') as f:
-                f.write(CCODE)
-            os.mkdir('objects')
+            with open("test_openmp.c", "w") as cfile:
+                cfile.write(CCODE)
+            os.mkdir("objects")
             # Compile, test program
             openmp_flags = get_openmp_flag(ccompiler)
-            ccompiler.compile(['test_openmp.c'], output_dir='objects',
-                              extra_postargs=openmp_flags)
+            ccompiler.compile(
+                ["test_openmp.c"],
+                output_dir="objects",
+                extra_postargs=openmp_flags,
+            )
             # Link test program
-            extra_preargs = os.getenv('LDFLAGS', None)
+            extra_preargs = os.getenv("LDFLAGS", None)
             if extra_preargs is not None:
                 extra_preargs = extra_preargs.split(" ")
             else:
                 extra_preargs = []
             objects = glob.glob(
-                os.path.join('objects', '*' + ccompiler.obj_extension))
-            ccompiler.link_executable(objects, 'test_openmp',
-                                      extra_preargs=extra_preargs,
-                                      extra_postargs=openmp_flags)
+                os.path.join("objects", "*" + ccompiler.obj_extension)
+            )
+            ccompiler.link_executable(
+                objects,
+                "test_openmp",
+                extra_preargs=extra_preargs,
+                extra_postargs=openmp_flags,
+            )
             # Run test program
-            output = subprocess.check_output('./test_openmp')
-            output = output.decode(sys.stdout.encoding or 'utf-8').splitlines()
+            output = subprocess.check_output("./test_openmp")
+            output = output.decode(sys.stdout.encoding or "utf-8").splitlines()
             # Check test program output
-            if 'nthreads=' in output[0]:
-                nthreads = int(output[0].strip().split('=')[1])
-                openmp_supported = (len(output) == nthreads)
-                flags = openmp_flags
+            if "nthreads=" in output[0]:
+                nthreads = int(output[0].strip().split("=")[1])
+                openmp_supported = len(output) == nthreads
             else:
                 openmp_supported = False
-                flags = []
+                openmp_flags = []
         except (CompileError, LinkError, subprocess.CalledProcessError):
             openmp_supported = False
-            flags = []
+            openmp_flags = []
         finally:
-            os.chdir(start_dir)
-    return openmp_supported, flags
+            os.chdir(HERE)
+    return openmp_supported, openmp_flags
 
 
 # openmp ######################################################################
 
 
-if "--openmp" in sys.argv:
-    USE_OPENMP = True
-else:
-    USE_OPENMP = False
+USE_OPENMP = bool("--openmp" in sys.argv)
 
 if USE_OPENMP:
     # just check if wanted
-    CAN_USE_OPENMP, flags = check_openmp_support()
+    CAN_USE_OPENMP, FLAGS = check_openmp_support()
     if CAN_USE_OPENMP:
-        print('## GSTOOLS setup: OpenMP found.')
-        print("## OpenMP flags:", flags)
+        print("## GSTOOLS setup: OpenMP found.")
+        print("## OpenMP flags:", FLAGS)
     else:
-        print('## GSTOOLS setup: OpenMP not found.')
+        print("## GSTOOLS setup: OpenMP not found.")
 else:
     CAN_USE_OPENMP = False
-    print('## GSTOOLS setup: OpenMP not wanted by the user.')
-    flags = []
+    print("## GSTOOLS setup: OpenMP not wanted by the user.")
+    FLAGS = []
 
 USE_OPENMP = USE_OPENMP and CAN_USE_OPENMP
 
@@ -182,9 +183,11 @@ USE_OPENMP = USE_OPENMP and CAN_USE_OPENMP
 # python3 setup.py --openmp build_ext --inplace
 # pip install --global-option="--openmp" gstools
 class MPDistribution(Distribution):
+    """Distribution with --openmp as global option"""
+
     global_options = Distribution.global_options + [
-        ('openmp', None, "Flag to use openmp in the build"),
-        ]
+        ("openmp", None, "Flag to use openmp in the build")
+    ]
 
 
 # cython extensions ###########################################################
@@ -192,27 +195,27 @@ class MPDistribution(Distribution):
 
 EXT_MODULES = []
 
-extra_compile_args = flags
-extra_link_args = flags
+EXTRA_COMPILE_ARGS = FLAGS
+EXTRA_LINK_ARGS = FLAGS
 
-summator_ext = Extension(
+SUMMATOR_EXT = Extension(
     "gstools.field.summator",
-    [os.path.join('gstools', 'field', 'summator.pyx')],
+    [os.path.join("gstools", "field", "summator.pyx")],
     include_dirs=[numpy.get_include()],
-    extra_compile_args=extra_compile_args,
-    extra_link_args=extra_link_args,
+    extra_compile_args=EXTRA_COMPILE_ARGS,
+    extra_link_args=EXTRA_LINK_ARGS,
 )
-variogram_ext = Extension(
+VARIOGRAM_EXT = Extension(
     "gstools.variogram.estimator",
     [os.path.join("gstools", "variogram", "estimator.pyx")],
     include_dirs=[numpy.get_include()],
-    extra_compile_args=extra_compile_args,
-    extra_link_args=extra_link_args,
+    extra_compile_args=EXTRA_COMPILE_ARGS,
+    extra_link_args=EXTRA_LINK_ARGS,
 )
 
 EXT_MODULES += cythonize(
-    [variogram_ext, summator_ext],
-    #annotate=True
+    [VARIOGRAM_EXT, SUMMATOR_EXT],
+    # annotate=True
 )
 
 # This is the important part. By setting this compiler directive, cython will
@@ -247,23 +250,23 @@ CLASSIFIERS = [
     "Topic :: Utilities",
 ]
 
-setup_kw = {
-    "name": "gstools",
-    "version": VERSION,
-    "maintainer": "Lennart Schueler, Sebastian Mueller",
-    "maintainer_email": "info@geostat-framework.org",
-    "description": DOCLINES[0],
-    "long_description": README,
-    "long_description_content_type": "text/markdown",
-    "author": "Lennart Schueler, Sebastian Mueller",
-    "author_email": "info@geostat-framework.org",
-    "url": "https://github.com/GeoStat-Framework/GSTools",
-    "license": "GPL - see LICENSE",
-    "classifiers": CLASSIFIERS,
-    "platforms": ["Linux"],
-    "include_package_data": True,
-    "setup_requires": ["numpy>=1.13.0"],  # numpy imported in setup.py
-    "install_requires": [
+setup(
+    name="gstools",
+    version=VERSION,
+    maintainer="Lennart Schueler, Sebastian Mueller",
+    maintainer_email="info@geostat-framework.org",
+    description=DOCLINES[0],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Lennart Schueler, Sebastian Mueller",
+    author_email="info@geostat-framework.org",
+    url="https://github.com/GeoStat-Framework/GSTools",
+    license="GPL - see LICENSE",
+    classifiers=CLASSIFIERS,
+    platforms=["Linux"],
+    include_package_data=True,
+    setup_requires=["numpy>=1.13.0"],  # numpy imported in setup.py
+    install_requires=[
         "numpy>=1.13.0",
         "scipy>=0.19.1",
         "hankel>=0.3.6",
@@ -271,13 +274,9 @@ setup_kw = {
         "pyevtk",
         "six",
     ],
-    "extras_require": {
-        "krige": ["pykrige"],
-    },
-    "packages": find_packages(exclude=["tests*", "docs*"]),
-    "ext_modules": EXT_MODULES,
-    "include_dirs": [numpy.get_include()],
-    "distclass": MPDistribution,
-}
-
-setup(**setup_kw)
+    extras_require={"krige": ["pykrige"]},
+    packages=find_packages(exclude=["tests*", "docs*"]),
+    ext_modules=EXT_MODULES,
+    include_dirs=[numpy.get_include()],
+    distclass=MPDistribution,
+)
