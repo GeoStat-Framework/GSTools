Fixed a numerical bug in the incompr. rand. meth. summator

tests/test_incomprrandmeth.py
@@ -39,16 +39,16 @@ class TestIncomprRandMeth(unittest.TestCase):
 
     def test_unstruct_2d(self):
         modes = self.rm_2d(self.x_tuple, self.y_tuple)
-        self.assertAlmostEqual(modes[0, 0], 1.99100618)
-        self.assertAlmostEqual(modes[0, 1], 2.02471175)
-        self.assertAlmostEqual(modes[1, 1], -0.28979649)
+        self.assertAlmostEqual(modes[0, 0], 0.50751115)
+        self.assertAlmostEqual(modes[0, 1], 1.03291018)
+        self.assertAlmostEqual(modes[1, 1], -0.22003005)
 
     def test_unstruct_3d(self):
         modes = self.rm_3d(self.x_tuple, self.y_tuple, self.z_tuple)
         if MC_VER < 3:
-            self.assertAlmostEqual(modes[0, 1], 0.99476583)
-            self.assertAlmostEqual(modes[1, 0], 0.17039711)
-            self.assertAlmostEqual(modes[1, 1], -0.25210944)
+            self.assertAlmostEqual(modes[0, 1], 1.69734094)
+            self.assertAlmostEqual(modes[1, 0], -0.10457179)
+            self.assertAlmostEqual(modes[1, 1], -0.43476757)
         else:
             self.assertAlmostEqual(modes[0, 0], 2.41526352)
             self.assertAlmostEqual(modes[0, 1], 3.29398652)
@@ -74,6 +74,24 @@ class TestIncomprRandMeth(unittest.TestCase):
             self.assertAlmostEqual(modes[1, 0, 1, 1], 0.12813365)
             self.assertAlmostEqual(modes[1, 1, 0, 1], 0.01443056)
             self.assertAlmostEqual(modes[1, 1, 1, 1], -0.12304040)
+
+    def test_struct_unstruct(self):
+        x_grid = np.arange(0., 2., 1.)
+        y_grid = np.arange(0., 2., 1.)
+        x_tuple = np.array((0., 0., 1., 1.))
+        y_tuple = np.array((0., 1., 0., 1.))
+        unstr_modes = self.rm_2d(
+            x_tuple, y_tuple, mesh_type="unstructured"
+        )
+        str_modes = self.rm_2d(
+            x_grid, y_grid, mesh_type="structured"
+        )
+        for d in range(2):
+            k = 0
+            for i in range(len(x_grid)):
+                for j in range(len(y_grid)):
+                    self.assertAlmostEqual(str_modes[d, i, j], unstr_modes[d, k])
+                    k += 1
 
     def test_assertions(self):
         cov_model_1d = Gaussian(dim=1, var=1.5, len_scale=2.5, mode_no=100)