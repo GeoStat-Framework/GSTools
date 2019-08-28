38e48cc6e756fde2fb460034cd05fd49bc9a1bb7
MuellerSeb <mueller.seb@posteo.de>
2019-08-28 15:32:31 +0200

    add multiple examples

---
+++ examples/11_ordinary_kriging.py
@@ -0,0 +1,14 @@
+import numpy as np
+from gstools import Gaussian, krige
+# condtions
+cond_pos = [0.3, 1.9, 1.1, 3.3, 4.7]
+cond_val = [0.47, 0.56, 0.74, 1.47, 1.74]
+# resulting grid
+gridx = np.linspace(0.0, 15.0, 151)
+# spatial random field class
+model = Gaussian(dim=1, var=0.5, len_scale=2)
+krig = krige.Ordinary(model, cond_pos=[cond_pos], cond_val=cond_val)
+krig([gridx])
+ax = krig.plot()
+ax.scatter(cond_pos, cond_val, color="k", zorder=10, label="Conditions")
+ax.legend()
