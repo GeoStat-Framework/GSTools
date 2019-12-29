6ddc5a9ddb8f28907525db2d97fa4090bdef3434
MuellerSeb <mueller.seb@posteo.de>
2019-12-29 18:12:11 +0100

    Field: remove update_model routine

--- gstools/field/base.py
+++ gstools/field/base.py
@@ -375,7 +375,6 @@ class Field:
     def model(self, model):
         if isinstance(model, CovModel):
             self._model = model
-            self.update_model()
         else:
             raise ValueError(
                 "Field: 'model' is not an instance of 'gstools.CovModel'"
@@ -385,10 +384,6 @@ class Field:
     def value_type(self):
         """:class:`str`: Type of the field values (scalar, vector)."""
         return self._value_type
-
-    def update_model(self):
-        """Can be overridden if anything needs to be updated with new model."""
-        pass
 
     def __str__(self):
         """Return String representation."""
