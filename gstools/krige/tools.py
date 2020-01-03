90dfe9956c47359d11545536a23e7858c5d15c4c
MuellerSeb <mueller.seb@posteo.de>
2020-01-03 15:58:44 +0100

    Krige: add eval_func to tools

--- gstools/krige/tools.py
+++ gstools/krige/tools.py
@@ -14,12 +14,45 @@ The following classes and functions are provided
 
 import numpy as np
 from gstools.tools.geometric import pos2xyz, xyz2pos
+from gstools.field.tools import (
+    reshape_axis_from_struct_to_unstruct,
+    reshape_field_from_unstruct_to_struct,
+)
 
 __all__ = ["set_condition", "get_drift_functions"]
 
 
+def eval_func(func, pos, mesh_type="structured"):
+    """
+    Evaluate a function on a mesh.
+
+    Parameters
+    ----------
+    func : :any:`callable`
+        The function to be called. Should have the signiture f(x, [y, z])
+    pos : :class:`list`
+        the position tuple, containing main direction and transversal
+        directions (x, [y, z])
+    mesh_type : :class:`str`, optional
+        'structured' / 'unstructured'
+
+    Returns
+    -------
+    :class:`numpy.ndarray`
+        Function values at the given points.
+    """
+    x, y, z, dim = pos2xyz(pos, calc_dim=True)
+    if mesh_type != "structured":
+        x, y, z, ax_lens = reshape_axis_from_struct_to_unstruct(dim, x, y, z)
+    res = func(*[x, y, z][:dim])
+    if mesh_type != "structured":
+        res = reshape_field_from_unstruct_to_struct(dim, res, ax_lens)
+    return res
+
+
 def set_condition(cond_pos, cond_val, max_dim=3):
-    """Set the conditions for kriging.
+    """
+    Set the conditions for kriging.
 
     Parameters
     ----------
@@ -29,6 +62,11 @@ def set_condition(cond_pos, cond_val, max_dim=3):
         the values of the conditions
     max_dim : :class:`int`, optional
         Cut of information above the given dimension. Default: 3
+
+    Raises
+    ------
+    ValueError
+        If the given data does not match the given dimension.
 
     Returns
     -------
@@ -40,6 +78,11 @@ def set_condition(cond_pos, cond_val, max_dim=3):
     # convert the input for right shapes and dimension checks
     c_x, c_y, c_z = pos2xyz(cond_pos, dtype=np.double, max_dim=max_dim)
     cond_pos = xyz2pos(c_x, c_y, c_z)
+    if len(cond_pos) != max_dim:
+        raise ValueError(
+            "Please check your 'cond_pos' parameters. "
+            + "The dimension does not match with the given one."
+        )
     cond_val = np.array(cond_val, dtype=np.double).reshape(-1)
     if not all([len(cond_pos[i]) == len(cond_val) for i in range(max_dim)]):
         raise ValueError(
