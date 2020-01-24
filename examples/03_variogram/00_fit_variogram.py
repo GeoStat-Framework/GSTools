443eb3ff52fb7b58aae7a91f23f4d7b4416b4065
MuellerSeb <mueller.seb@posteo.de>
2020-01-24 23:30:44 +0100

    Examples: update 03_variogram

--- examples/03_variogram/00_fit_variogram.py
+++ examples/03_variogram/00_fit_variogram.py
@@ -3,21 +3,32 @@ Fit Variogram
 -------------
 """
 import numpy as np
-from gstools import SRF, Exponential, Stable, vario_estimate_unstructured
+import gstools as gs
 
-# generate a synthetic field with an exponential model
+###############################################################################
+# Generate a synthetic field with an exponential model.
+
 x = np.random.RandomState(19970221).rand(1000) * 100.0
 y = np.random.RandomState(20011012).rand(1000) * 100.0
-model = Exponential(dim=2, var=2, len_scale=8)
-srf = SRF(model, mean=0, seed=19970221)
+model = gs.Exponential(dim=2, var=2, len_scale=8)
+srf = gs.SRF(model, mean=0, seed=19970221)
 field = srf((x, y))
-# estimate the variogram of the field with 40 bins
+
+###############################################################################
+# Estimate the variogram of the field with 40 bins.
+
 bins = np.arange(40)
-bin_center, gamma = vario_estimate_unstructured((x, y), field, bins)
-# fit the variogram with a stable model. (no nugget fitted)
-fit_model = Stable(dim=2)
+bin_center, gamma = gs.vario_estimate_unstructured((x, y), field, bins)
+
+###############################################################################
+# Fit the variogram with a stable model (no nugget fitted).
+
+fit_model = gs.Stable(dim=2)
 fit_model.fit_variogram(bin_center, gamma, nugget=False)
-# output
+
+###############################################################################
+# Plot the fitting result.
+
 ax = fit_model.plot(x_max=40)
 ax.plot(bin_center, gamma)
 print(fit_model)
