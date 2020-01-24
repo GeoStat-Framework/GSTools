df1719cdd7c107f21d1b33ea55a4ff522a3e9f06
MuellerSeb <mueller.seb@posteo.de>
2020-01-24 23:14:45 +0100

    Examples: update 00_misc

--- examples/00_misc/01_export.py
+++ examples/00_misc/01_export.py
@@ -3,10 +3,10 @@ Export
 ------
 """
 
-from gstools import SRF, Gaussian
+import gstools as gs
 
 x = y = range(100)
-model = Gaussian(dim=2, var=1, len_scale=10)
-srf = SRF(model)
+model = gs.Gaussian(dim=2, var=1, len_scale=10)
+srf = gs.SRF(model)
 field = srf((x, y), mesh_type="structured")
-srf.vtk_export("field")
+srf.vtk_export(filename="field")
