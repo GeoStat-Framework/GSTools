ee2a4c9fe2189dfe7b82c7c7ac4f06f181966199
MuellerSeb <mueller.seb@posteo.de>
2020-01-25 01:47:36 +0100

    Examples: update 02_cov_model

---
+++ examples/02_cov_model/04_different_scales.py
@@ -0,0 +1,56 @@
+r"""
+Different scales
+================
+
+Besides the length-scale, there are many other ways of characterizing a certain
+scale of a covariance model. We provide two common scales with the covariance
+model.
+
+Integral scale
+--------------
+
+The `integral scale <https://en.wikipedia.org/wiki/Integral_length_scale>`_
+of a covariance model is calculated by:
+
+.. math:: I = \int_0^\infty \mathrm{cor}(r) dr
+
+You can access it by:
+"""
+import gstools as gs
+
+model = gs.Stable(dim=3, var=2.0, len_scale=10)
+print("Main integral scale:", model.integral_scale)
+print("All integral scales:", model.integral_scale_vec)
+
+
+###############################################################################
+# You can also specify integral length scales like the ordinary length scale,
+# and len_scale/anis will be recalculated:
+
+model = gs.Stable(dim=3, var=2.0, integral_scale=[10, 4, 2])
+print("Anisotropy ratios:", model.anis)
+print("Main length scale:", model.len_scale)
+print("All length scales:", model.len_scale_vec)
+print("Main integral scale:", model.integral_scale)
+print("All integral scales:", model.integral_scale_vec)
+
+
+###############################################################################
+# Percentile scale
+# ----------------
+#
+# Another scale characterizing the covariance model, is the percentile scale.
+# It is the distance, where the normalized
+# variogram reaches a certain percentage of its sill.
+
+model = gs.Stable(dim=3, var=2.0, len_scale=10)
+per_scale = model.percentile_scale(0.9)
+print("Percentile scale:", per_scale)
+ax = model.plot()
+ax.axhline(1.8, color="k", linestyle=":", label=r"90% percentile")
+ax.legend()
+
+###############################################################################
+# .. note::
+#
+#    The nugget is neglected by this percentile_scale.
