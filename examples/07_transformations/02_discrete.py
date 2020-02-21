a4446c154d3bc8cf6281b47f060e2d76ea6211ce
LSchueler <mostem@posteo.de>
2020-02-21 11:38:00 +0100

    Remove unnecessary input check and add an example
    Now, arbitrary values can be assigned to the value classes which are
    separated by the thresholds.

--- examples/07_transformations/02_discrete.py
+++ examples/07_transformations/02_discrete.py
@@ -2,9 +2,10 @@
 discrete fields
 -------------
 
-Here we transform a field to a discrete field with five values.
+Here we transform a field to a discrete field with values.
 If we do not give thresholds, the pairwise means of the given
 values are taken as thresholds.
+If thresholds are given, arbitrary values can be applied to the field.
 """
 import numpy as np
 import gstools as gs
@@ -17,4 +18,13 @@ srf.structured([x, y])
 # create 5 equidistanly spaced values
 discrete_values = np.linspace(np.min(srf.field), np.max(srf.field), 5)
 gs.transform.discrete(srf, discrete_values)
+srf.plot()
+
+# use 3 thresholds for separation
+thresholds = [-1, 0, 1]
+# but apply different values to the separated classes
+discrete_values2 = [0, 10, 100, 1000]
+
+srf.structured([x, y])
+gs.transform.discrete(srf, discrete_values2, thresholds)
 srf.plot()
