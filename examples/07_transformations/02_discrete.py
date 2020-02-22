4a8c9b8a3330d695c281ac947501afd85339b5ae
MuellerSeb <mueller.seb@posteo.de>
2020-02-22 15:35:04 +0100

    examples: add equal thresholds example for discrete transform

--- examples/07_transformations/02_discrete.py
+++ examples/07_transformations/02_discrete.py
@@ -10,21 +10,28 @@ If thresholds are given, arbitrary values can be applied to the field.
 import numpy as np
 import gstools as gs
 
-# structured field with a size of 100x100 and a grid-size of 1x1
-x = y = range(100)
-model = gs.Gaussian(dim=2, var=1, len_scale=10)
+# structured field with a size of 100x100 and a grid-size of 0.5x0.5
+x = y = np.arange(200) * 0.5
+model = gs.Gaussian(dim=2, var=1, len_scale=5)
 srf = gs.SRF(model, seed=20170519)
-srf.structured([x, y])
-# create 5 equidistanly spaced values
+
+# create 5 equidistanly spaced values, thresholds are the arithmetic means
 discrete_values = np.linspace(np.min(srf.field), np.max(srf.field), 5)
+srf.structured([x, y])
 gs.transform.discrete(srf, discrete_values)
 srf.plot()
 
-# use 3 thresholds for separation
-thresholds = [-1, 0, 1]
+# calculate thresholds for equal shares
 # but apply different values to the separated classes
-discrete_values2 = [0, 10, 100, 1000]
-
+discrete_values2 = [0, -1, 2, -3, 4]
 srf.structured([x, y])
-gs.transform.discrete(srf, discrete_values2, thresholds)
+gs.transform.discrete(srf, discrete_values2, thresholds="equal")
+srf.plot()
+
+# user defined thresholds
+thresholds = [-1, 1]
+# apply different values to the separated classes
+discrete_values3 = [0, 1, 10]
+srf.structured([x, y])
+gs.transform.discrete(srf, discrete_values3, thresholds=thresholds)
 srf.plot()
