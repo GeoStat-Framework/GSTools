947580b44037e34109f73498d617316c59077d80
MuellerSeb <mueller.seb@posteo.de>
2020-03-22 12:08:37 +0100

    Bugfix: ModuleNotFoundError not present in py35

--- gstools/__init__.py
+++ gstools/__init__.py
@@ -131,7 +131,7 @@ from gstools.covmodel import (
 
 try:
     from gstools._version import __version__
-except ModuleNotFoundError:  # pragma: nocover
+except ImportError:  # pragma: nocover
     # package is not installed
     __version__ = "0.0.0.dev0"
 
