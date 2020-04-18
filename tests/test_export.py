a01abbbb460f779134ee76491e7898c055f3b454
LSchueler <mostem@posteo.de>
2020-04-18 10:41:03 +0200

    Refactor export test

--- tests/test_export.py
+++ tests/test_export.py
@@ -6,8 +6,7 @@ import os
 import tempfile
 import shutil
 
-from gstools import SRF, Gaussian, Exponential
-from gstools.random import MasterRNG
+import gstools as gs
 
 HAS_PYVISTA = False
 try:
@@ -21,41 +20,46 @@ except ImportError:
 class TestExport(unittest.TestCase):
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
-        x = y = z = range(25)
-        model = Gaussian(dim=3, var=0.6, len_scale=20)
-        self.srf_structured = SRF(model)
-        self.srf_structured((x, y, z), mesh_type="structured")
-        # unstrucutred field
-        seed = MasterRNG(19970221)
+        self.x_grid = self.y_grid = self.z_grid = range(25)
+        model = gs.Gaussian(dim=3, var=0.6, len_scale=20)
+        self.srf_structured = gs.SRF(model)
+        self.srf_structured(
+            (self.x_grid, self.y_grid, self.z_grid), mesh_type="structured"
+        )
+        # unstructured field
+        seed = gs.random.MasterRNG(19970221)
         rng = np.random.RandomState(seed())
-        x = rng.randint(0, 100, size=100)
-        y = rng.randint(0, 100, size=100)
-        model = Exponential(
+        self.x_tuple = rng.randint(0, 100, size=100)
+        self.y_tuple = rng.randint(0, 100, size=100)
+        model = gs.Exponential(
             dim=2, var=1, len_scale=[12.0, 3.0], angles=np.pi / 8.0
         )
-        self.srf_unstructured = SRF(model, seed=20170519)
-        self.srf_unstructured([x, y])
+        self.srf_unstructured = gs.SRF(model, seed=20170519)
+        self.srf_unstructured([self.x_tuple, self.y_tuple])
 
     def tearDown(self):
         # Remove the test data directory after the test
         shutil.rmtree(self.test_dir)
 
     @unittest.skipIf(not HAS_PYVISTA, "PyVista is not installed.")
-    def test_pyvista(self):
+    def test_pyvista_struct(self):
         mesh = self.srf_structured.to_pyvista()
         self.assertIsInstance(mesh, pv.RectilinearGrid)
+
+    @unittest.skipIf(not HAS_PYVISTA, "PyVista is not installed.")
+    def test_pyvista_unstruct(self):
         mesh = self.srf_unstructured.to_pyvista()
         self.assertIsInstance(mesh, pv.UnstructuredGrid)
 
-    def test_pyevtk_export(self):
-        # Structured
-        sfilename = os.path.join(self.test_dir, "structured")
-        self.srf_structured.vtk_export(sfilename)
-        self.assertTrue(os.path.isfile(sfilename + ".vtr"))
-        # Unstructured
-        ufilename = os.path.join(self.test_dir, "unstructured")
-        self.srf_unstructured.vtk_export(ufilename)
-        self.assertTrue(os.path.isfile(ufilename + ".vtu"))
+    def test_pyevtk_export_struct(self):
+        filename = os.path.join(self.test_dir, "structured")
+        self.srf_structured.vtk_export(filename)
+        self.assertTrue(os.path.isfile(filename + ".vtr"))
+
+    def test_pyevtk_export_unstruct(self):
+        filename = os.path.join(self.test_dir, "unstructured")
+        self.srf_unstructured.vtk_export(filename)
+        self.assertTrue(os.path.isfile(filename + ".vtu"))
 
 
 if __name__ == "__main__":
