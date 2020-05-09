4f64b85a7002bec1c8b73911058383ef7ac93d30
Tobi Glaubach <tobias.glaubach@gmail.com>
2020-05-09 12:11:31 +0200

    changed for automatic testcase data generation and split up the test cases

--- tests/test_variogram_unstructured.py
+++ tests/test_variogram_unstructured.py
@@ -5,12 +5,38 @@ This is a unittest of the variogram module.
 
 import unittest
 import numpy as np
-from gstools import vario_estimate_unstructured
-
+from gstools import vario_estimate_unstructured, Exponential, SRF
+import gstools as gs
 
 class TestVariogramUnstructured(unittest.TestCase):
     def setUp(self):
-        pass
+
+        # this code generates the testdata for the 2D rotation test cases
+        x = np.random.RandomState(19970221).rand(20) * 10.0 - 5
+        y = np.zeros_like(x)
+        model = gs.Exponential(dim=2, var=2, len_scale=8)
+        srf = gs.SRF(model, mean=0, seed=19970221)
+        field = srf((x, y))
+
+        bins = np.arange(10)
+        bin_center, gamma = gs.vario_estimate_unstructured((x, ), field, bins)
+        idx = np.argsort(x)
+        self.test_data_rotation_1 = {'gamma': gamma, 'x': x[idx], 'field': field[idx], 'bins': bins, 'bin_center': bin_center}
+
+        # CODE ABOVE SHOULD GENERATE THIS DATA
+        # x = np.array([
+        # -4.86210059, -4.1984934 , -3.9246953 , -3.28490663, -2.16332379,
+        # -1.87553275, -1.74125124, -1.27224687, -1.20931578, -0.2413368 ,
+        #  0.03200921,  1.17099773,  1.53863105,  1.64478688,  2.75252136,
+        #  3.3556915 ,  3.89828775,  4.21485964,  4.5364357 ,  4.79236969]),
+        # field = np.array([
+        # -1.10318365, -0.53566629, -0.41789049, -1.06167529,  0.38449961,
+        # -0.36550477, -0.98905552, -0.19352766,  0.16264266,  0.26920833,
+        #  0.05379665,  0.71275006,  0.36651935,  0.17366865,  1.20022343,
+        #  0.79385446,  0.69456069,  1.0733393 ,  0.71191592,  0.71969766])
+        # gamma_exp = np.array([
+        # 0.14260989, 0.18301197, 0.25855841, 0.29990083, 0.67914526,
+        # 0.60136535, 0.92875492, 1.46910435, 1.10165104])
 
     def test_doubles(self):
         x = np.arange(1, 11, 1, dtype=np.double)
@@ -217,24 +243,12 @@ class TestVariogramUnstructured(unittest.TestCase):
             ValueError, vario_estimate_unstructured, [x], field_e, bins
         )
 
-    def test_angles(self):
-        x = np.array([
-        -4.86210059, -4.1984934 , -3.9246953 , -3.28490663, -2.16332379,
-        -1.87553275, -1.74125124, -1.27224687, -1.20931578, -0.2413368 ,
-         0.03200921,  1.17099773,  1.53863105,  1.64478688,  2.75252136,
-         3.3556915 ,  3.89828775,  4.21485964,  4.5364357 ,  4.79236969]),
-        field = np.array([
-        -1.10318365, -0.53566629, -0.41789049, -1.06167529,  0.38449961,
-        -0.36550477, -0.98905552, -0.19352766,  0.16264266,  0.26920833,
-         0.05379665,  0.71275006,  0.36651935,  0.17366865,  1.20022343,
-         0.79385446,  0.69456069,  1.0733393 ,  0.71191592,  0.71969766])
-        
-        gamma_exp = np.array([
-        0.14260989, 0.18301197, 0.25855841, 0.29990083, 0.67914526,
-        0.60136535, 0.92875492, 1.46910435, 1.10165104])
-        
-        bins = np.arange(0, 10)
-        
+    def test_angles_2D_x2x(self):
+
+        x = self.test_data_rotation_1['x']
+        field = self.test_data_rotation_1['field']
+        gamma_exp = self.test_data_rotation_1['gamma']
+        bins = self.test_data_rotation_1['bins']
         y = np.zeros_like(x)
         
         # test case 1.)
@@ -250,6 +264,13 @@ class TestVariogramUnstructured(unittest.TestCase):
         for i in range(gamma.size):
             self.assertAlmostEqual(gamma_exp[i], gamma[i], places=3)
 
+    def test_angles_2D_y2x(self):
+
+        x = self.test_data_rotation_1['x']
+        field = self.test_data_rotation_1['field']
+        gamma_exp = self.test_data_rotation_1['gamma']
+        bins = self.test_data_rotation_1['bins']
+        y = np.zeros_like(x)
 
         # test case 2.)
         #    all along y axis on y axis but calculation for x axis
@@ -264,6 +285,14 @@ class TestVariogramUnstructured(unittest.TestCase):
         for i in range(gamma.size):
             self.assertAlmostEqual(0, gamma[i], places=3)
             
+    def test_angles_2D_y2y(self):
+
+        x = self.test_data_rotation_1['x']
+        field = self.test_data_rotation_1['field']
+        gamma_exp = self.test_data_rotation_1['gamma']
+        bins = self.test_data_rotation_1['bins']
+        y = np.zeros_like(x)
+
         # test case 3.)
         #    all along y axis on y axis and calculation for y axis
             
@@ -276,7 +305,16 @@ class TestVariogramUnstructured(unittest.TestCase):
         
         for i in range(gamma.size):
             self.assertAlmostEqual(gamma_exp[i], gamma[i], places=3)
-            
+
+    def test_angles_2D_xy2x(self):
+
+        x = self.test_data_rotation_1['x']
+        field = self.test_data_rotation_1['field']
+        gamma_exp = self.test_data_rotation_1['gamma']
+        bins = self.test_data_rotation_1['bins']
+        y = np.zeros_like(x)
+
+
         # test case 4.)
         #    data along 45deg axis but calculation for x axis
             
@@ -295,6 +333,14 @@ class TestVariogramUnstructured(unittest.TestCase):
         for i in range(gamma.size):
             self.assertAlmostEqual(0, gamma[i], places=3)
             
+    def test_angles_2D_xy2xy(self):
+
+        x = self.test_data_rotation_1['x']
+        field = self.test_data_rotation_1['field']
+        gamma_exp = self.test_data_rotation_1['gamma']
+        bins = self.test_data_rotation_1['bins']
+        y = np.zeros_like(x)
+
         # test case 5.)
         #    data along 45deg axis and calculation for 45deg
             
