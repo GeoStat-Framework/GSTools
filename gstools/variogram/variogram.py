536858c8fa4055d77907ff08de1a74bc81a3c1c4
MuellerSeb <mueller.seb@posteo.de>
2020-11-05 23:08:35 +0100

    vario: doc update; correct intervals for angles; general formatting of angles array

--- gstools/variogram/variogram.py
+++ gstools/variogram/variogram.py
@@ -42,7 +42,7 @@ def vario_estimate_unstructured(
     sampling_size=None,
     sampling_seed=None,
     estimator="matheron",
-    return_counts=False
+    return_counts=False,
 ):
     r"""
     Estimates the variogram on a unstructured grid.
@@ -53,7 +53,8 @@ def vario_estimate_unstructured(
        \gamma(r_k) = \frac{1}{2 N(r_k)} \sum_{i=1}^{N(r_k)} (z(\mathbf x_i) -
        z(\mathbf x_i'))^2 \; ,
 
-    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}` being the bins.
+    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}`
+    being the bins.
 
     Or if the estimator "cressie" was chosen:
 
@@ -62,12 +63,9 @@ def vario_estimate_unstructured(
        \left|z(\mathbf x_i) - z(\mathbf x_i')\right|^{0.5}\right)^4}
        {0.457 + 0.494 / N(r_k) + 0.045 / N^2(r_k)} \; ,
 
-    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}` being the bins.
+    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}`
+    being the bins.
     The Cressie estimator is more robust to outliers.
-
-    Notes
-    -----
-    Internally uses double precision and also returns doubles.
 
     Parameters
     ----------
@@ -80,15 +78,15 @@ def vario_estimate_unstructured(
         the bins on which the variogram will be calculated
     angles : :class:`numpy.ndarray`
         the angles of the main axis to calculate the variogram for in radians
-        angle definitions from ISO standard 80000-2:2009 
+        angle definitions from ISO standard 80000-2:2009
         for 1d this parameter will have no effect at all
         for 2d supply one angle which is azimuth φ (ccw from +x in xy plane)
-        for 3d supply two angles which are inclination θ (cw from +z)
-        and azimuth φ (ccw from +x in xy plane)
+        for 3d supply two angles which are azimuth φ (ccw from +x in xy plane)
+        and inclination θ (cw from +z)
     angles_tol : class:`float`
         the tolerance around the variogram angle to count a point as being
         within this direction from another point (the angular tolerance around
-        the directional vector given by angles) 
+        the directional vector given by angles)
         Default: 25°≈0.436332
     sampling_size : :class:`int` or :any:`None`, optional
         for large input data, this method can take a long
@@ -106,32 +104,42 @@ def vario_estimate_unstructured(
 
         Default: "matheron"
     return_counts: class:`bool`, optional
-        if set to true, this function will also return the number of data 
+        if set to true, this function will also return the number of data
         points found at each lag distance as a third return value
         Default: False
+
     Returns
     -------
     :class:`tuple` of :class:`numpy.ndarray`
         1. the bin centers
         2. the estimated variogram values at bin centers
-        (optional) 3. the number of points found at each bin center (see argument return_counts)
+        3. (optional) the number of points found at each bin center
+           (see argument return_counts)
+
+    Notes
+    -----
+    Internally uses double precision and also returns doubles.
     """
     # TODO check_mesh
     field = np.array(field, ndmin=1, dtype=np.double)
     bin_edges = np.array(bin_edges, ndmin=1, dtype=np.double)
     x, y, z, dim = pos2xyz(pos, calc_dim=True, dtype=np.double)
-    
+
     bin_centres = (bin_edges[:-1] + bin_edges[1:]) / 2.0
-    
+
     if angles is not None:
-        angles = np.array(angles, ndmin=1, dtype=np.double)
-        if angles.size == 0:
-            angles = np.append(angles, [0,0,0])
-        elif angles.size == 1:
-            angles = np.append(angles, [0,0])
-        elif angles.size == 2:
-            angles = np.append(angles, [0])
-            
+        # there are at most (dim-1) angles
+        angles = np.array(angles, ndmin=1, dtype=np.double).ravel()[
+            : (dim - 1)
+        ]
+        angles = np.pad(
+            angles, (0, dim - angles.size - 1), "constant", constant_values=0.0
+        )
+        # correct intervalls for angles (prepared for n-d)
+        if dim >= 2:
+            angles[0] = angles[0] % (2 * np.pi)  # azimuth in [0, 2pi)
+        if dim >= 3:
+            angles[1:] = angles[1:] % np.pi  # inclinations in [0, pi)
 
     if sampling_size is not None and sampling_size < len(field):
         sampled_idx = np.random.RandomState(sampling_seed).choice(
@@ -147,20 +155,20 @@ def vario_estimate_unstructured(
     cython_estimator = _set_estimator(estimator)
 
     estimates, counts = unstructured(
-            field, bin_edges, x, y, z, angles, angles_tol, estimator_type=cython_estimator
-        )
+        field,
+        bin_edges,
+        x,
+        y,
+        z,
+        angles,
+        angles_tol,
+        estimator_type=cython_estimator,
+    )
 
     if return_counts:
-        return (
-            bin_centres,
-            estimates,
-            counts
-        )
+        return bin_centres, estimates, counts
     else:
-        return (
-            bin_centres,
-            estimates
-        )
+        return bin_centres, estimates
 
 
 def vario_estimate_structured(field, direction="x", estimator="matheron"):
@@ -173,7 +181,8 @@ def vario_estimate_structured(field, direction="x", estimator="matheron"):
        \gamma(r_k) = \frac{1}{2 N(r_k)} \sum_{i=1}^{N(r_k)} (z(\mathbf x_i) -
        z(\mathbf x_i'))^2 \; ,
 
-    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}` being the bins.
+    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}`
+    being the bins.
 
     Or if the estimator "cressie" was chosen:
 
@@ -182,16 +191,9 @@ def vario_estimate_structured(field, direction="x", estimator="matheron"):
        \left|z(\mathbf x_i) - z(\mathbf x_i')\right|^{0.5}\right)^4}
        {0.457 + 0.494 / N(r_k) + 0.045 / N^2(r_k)} \; ,
 
-    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}` being the bins.
+    with :math:`r_k \leq \| \mathbf x_i - \mathbf x_i' \| < r_{k+1}`
+    being the bins.
     The Cressie estimator is more robust to outliers.
-
-    Warnings
-    --------
-    It is assumed that the field is defined on an equidistant Cartesian grid.
-
-    Notes
-    -----
-    Internally uses double precision and also returns doubles.
 
     Parameters
     ----------
@@ -211,6 +213,14 @@ def vario_estimate_structured(field, direction="x", estimator="matheron"):
     -------
     :class:`numpy.ndarray`
         the estimated variogram along the given direction.
+
+    Warnings
+    --------
+    It is assumed that the field is defined on an equidistant Cartesian grid.
+
+    Notes
+    -----
+    Internally uses double precision and also returns doubles.
     """
     try:
         mask = np.array(field.mask, dtype=np.int32)
