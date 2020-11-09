f0e9c610b0e3f594db1c56b488d199c9a55c950c
MuellerSeb <mueller.seb@posteo.de>
2020-11-10 00:55:55 +0100

    examples: 2d example for directional variogram

---
+++ examples/03_variogram/04_directional_2d.py
@@ -0,0 +1,48 @@
+"""
+Directional variogram estimation in 2D
+--------------------------------------
+
+In this example, we demonstrate how to estimate a directional variogram by
+setting the direction angles in 2D.
+"""
+import numpy as np
+import gstools as gs
+from matplotlib import pyplot as plt
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=[10, 5])
+
+###############################################################################
+# Generating synthetic field with anisotropy and a rotation of 22.5 degree.
+
+angle = np.pi / 8
+model = gs.Exponential(dim=2, len_scale=[10, 5], angles=angle)
+x = y = range(100)
+srf = gs.SRF(model, seed=123456)
+field = srf((x, y), mesh_type="structured")
+srf.plot(ax=ax2)
+ax2.set_aspect("equal")
+
+###############################################################################
+# Now we are going to estimate a directional variogram with an angular
+# tolerance of 11.25 degree and a bandwith of 1.
+# We provide the rotation angle of the covariance model and the orthogonal
+# direction by adding 90 degree.
+
+bins = range(0, 40, 3)
+bin_c, vario, cnt = gs.vario_estimate_unstructured(
+    *((x, y), field, bins),
+    angles=(angle, angle + np.pi / 2),
+    angles_tol=np.pi / 16,
+    bandwidth=1.0,
+    mesh_type="structured",
+    return_counts=True,
+)
+ax1.plot(bin_c, vario[0], label="emp. vario: pi/8")
+ax1.plot(bin_c, vario[1], label="emp. vario: pi*5/8")
+ax1.legend(loc="lower right")
+fig.show()
+
+###############################################################################
+# Without fitting a model we see, that the correlation length in the main
+# direction is greater than the transversal one.
