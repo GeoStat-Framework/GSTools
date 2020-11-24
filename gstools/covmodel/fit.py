a1170a2eba5a059a47c65ff8ce6c060c893fbf92
MuellerSeb <mueller.seb@posteo.de>
2020-11-25 00:14:48 +0100

    CovModel.fit: allow geographical estimated variograms for fitting

--- gstools/covmodel/fit.py
+++ gstools/covmodel/fit.py
@@ -286,6 +286,13 @@ def _check_vario(model, x_data, y_data):
             + "Either provide only one variogram to fit an isotropic model, "
             + "or directional ones for all main axes to fit anisotropy."
         )
+    if is_dir_vario and model.latlon:
+        raise ValueError(
+            "CovModel.fit_variogram: lat-lon models don't support anisotropy."
+        )
+    elif model.latlon:
+        # convert to yadrenko model
+        x_data = 2 * np.sin(x_data / 2)
     return x_data, y_data, is_dir_vario
 
 
