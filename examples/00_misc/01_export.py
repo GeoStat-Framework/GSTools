87cc84d023e8ffe3901be16365bf863271532e88
MuellerSeb <mueller.seb@posteo.de>
2021-01-16 08:33:17 +0100

    Examples: add paraview img to export example

--- examples/00_misc/01_export.py
+++ examples/00_misc/01_export.py
@@ -1,8 +1,13 @@
 """
 Exporting Fields
 ----------------
-"""
 
+GSTools provides simple exporting routines to convert generated fields to
+`VTK <https://vtk.org/>`__ files.
+
+These can be viewed for example with `Paraview <https://www.paraview.org/>`__.
+"""
+# sphinx_gallery_thumbnail_path = 'pics/paraview.png'
 import gstools as gs
 
 x = y = range(100)
@@ -10,3 +15,10 @@ model = gs.Gaussian(dim=2, var=1, len_scale=10)
 srf = gs.SRF(model)
 field = srf((x, y), mesh_type="structured")
 srf.vtk_export(filename="field")
+
+###############################################################################
+# The result displayed with Paraview:
+#
+# .. image:: https://raw.githubusercontent.com/GeoStat-Framework/GeoStat-Framework.github.io/master/img/paraview.png
+#    :width: 400px
+#    :align: center
