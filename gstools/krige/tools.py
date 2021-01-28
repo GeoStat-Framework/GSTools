185bc0c53083effe6ce336ed69a8cf5b3dd0edef
MuellerSeb <mueller.seb@posteo.de>
2021-01-28 17:24:42 +0100

    Krige: remove moved routines

--- gstools/krige/tools.py
+++ gstools/krige/tools.py
@@ -9,66 +9,13 @@ The following classes and functions are provided
 .. autosummary::
    set_condition
    get_drift_functions
-   no_trend
-   eval_func
 """
 # pylint: disable=C0103
 from itertools import combinations_with_replacement
 import numpy as np
-from gstools.tools.geometric import format_struct_pos_dim, gen_mesh
 
 
-__all__ = ["no_trend", "eval_func", "set_condition", "get_drift_functions"]
-
-
-def no_trend(*args, **kwargs):
-    """
-    Zero trend dummy function.
-
-    Parameters
-    ----------
-    *args : any
-        Ignored arguments.
-    **kwargs : any
-        Ignored keyword arguments.
-
-    Returns
-    -------
-    float
-        A zero trend given as single float.
-
-    """
-    return 0.0
-
-
-def eval_func(func, pos, dim, mesh_type="structured"):
-    """
-    Evaluate a function on a mesh.
-
-    Parameters
-    ----------
-    func : :any:`callable`
-        The function to be called. Should have the signiture f(x, [y, z, ...]).
-    pos : :class:`list`
-        The position tuple, containing main direction and transversal
-        directions (x, [y, z, ...]).
-    dim : :class:`int`
-        The spatial dimension.
-    mesh_type : :class:`str`, optional
-        'structured' / 'unstructured'
-
-    Returns
-    -------
-    :class:`numpy.ndarray`
-        Function values at the given points.
-    """
-    if mesh_type != "unstructured":
-        pos, shape = format_struct_pos_dim(pos, dim)
-        pos = gen_mesh(pos)
-    else:
-        pos = np.array(pos, dtype=np.double).reshape(dim, -1)
-        shape = np.shape(pos[0])
-    return np.reshape(func(*pos), shape)
+__all__ = ["set_condition", "get_drift_functions"]
 
 
 def set_condition(cond_pos, cond_val, dim):
@@ -102,7 +49,7 @@ def set_condition(cond_pos, cond_val, dim):
     if len(cond_pos[0]) != len(cond_val):
         raise ValueError(
             "Please check your 'cond_pos' and 'cond_val' parameters. "
-            + "The shapes do not match."
+            "The shapes do not match."
         )
     return cond_pos, cond_val
 
