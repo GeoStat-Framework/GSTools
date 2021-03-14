a57637cfcbfdb42f954921068eba146086066d14
Sebastian Müller <mueller.seb@posteo.de>
2021-03-14 14:35:44 +0100

    Examples: use new plotting; simplify variogram estimation

--- examples/03_variogram/04_directional_3d.py
+++ examples/03_variogram/04_directional_3d.py
@@ -17,11 +17,12 @@ from mpl_toolkits.mplot3d import Axes3D
 
 dim = 3
 # rotation around z, y, x
-angles = [np.pi / 2, np.pi / 4, np.pi / 8]
+angles = [np.deg2rad(90), np.deg2rad(45), np.deg2rad(22.5)]
 model = gs.Gaussian(dim=3, len_scale=[16, 8, 4], angles=angles)
 x = y = z = range(50)
+pos = (x, y, z)
 srf = gs.SRF(model, seed=1001)
-field = srf.structured((x, y, z))
+field = srf.structured(pos)
 
 ###############################################################################
 # Here we generate the axes of the rotated coordinate system
@@ -37,9 +38,9 @@ axis1, axis2, axis3 = main_axes
 # with the longest correlation length (flattest gradient).
 # Then check the transversal directions and so on.
 
-bins = range(0, 40, 3)
 bin_center, dir_vario, counts = gs.vario_estimate(
-    *([x, y, z], field, bins),
+    pos,
+    field,
     direction=main_axes,
     bandwidth=10,
     sampling_size=2000,
@@ -51,47 +52,45 @@ bin_center, dir_vario, counts = gs.vario_estimate(
 ###############################################################################
 # Afterwards we can use the estimated variogram to fit a model to it.
 # Note, that the rotation angles need to be set beforehand.
-#
-# We can use the `counts` of data pairs per bin as weights for the fitting
-# routines to give more attention to areas where more data was available.
-# In order to not introduce to much offset at the origin, we disable
-# fitting the nugget.
 
 print("Original:")
 print(model)
-model.fit_variogram(bin_center, dir_vario, weights=counts, nugget=False)
+model.fit_variogram(bin_center, dir_vario)
 print("Fitted:")
 print(model)
 
 ###############################################################################
-# Plotting.
+# Plotting main axes and the fitted directional variogram.
 
-fig = plt.figure(figsize=[15, 5])
-ax1 = fig.add_subplot(131)
-ax2 = fig.add_subplot(132, projection=Axes3D.name)
-ax3 = fig.add_subplot(133)
+fig = plt.figure(figsize=[10, 5])
+ax1 = fig.add_subplot(121, projection=Axes3D.name)
+ax2 = fig.add_subplot(122)
 
-srf.plot(ax=ax1)
+ax1.plot([0, axis1[0]], [0, axis1[1]], [0, axis1[2]], label="0.")
+ax1.plot([0, axis2[0]], [0, axis2[1]], [0, axis2[2]], label="1.")
+ax1.plot([0, axis3[0]], [0, axis3[1]], [0, axis3[2]], label="2.")
+ax1.set_xlim(-1, 1)
+ax1.set_ylim(-1, 1)
+ax1.set_zlim(-1, 1)
+ax1.set_xlabel("X")
+ax1.set_ylabel("Y")
+ax1.set_zlabel("Z")
+ax1.set_title("Tait-Bryan main axis")
+ax1.legend(loc="lower left")
 
-ax2.plot([0, axis1[0]], [0, axis1[1]], [0, axis1[2]], label="0.")
-ax2.plot([0, axis2[0]], [0, axis2[1]], [0, axis2[2]], label="1.")
-ax2.plot([0, axis3[0]], [0, axis3[1]], [0, axis3[2]], label="2.")
-ax2.set_xlim(-1, 1)
-ax2.set_ylim(-1, 1)
-ax2.set_zlim(-1, 1)
-ax2.set_xlabel("X")
-ax2.set_ylabel("Y")
-ax2.set_zlabel("Z")
-ax2.set_title("Tait-Bryan main axis")
-ax2.legend(loc="lower left")
-
-ax3.scatter(bin_center, dir_vario[0], label="0. axis")
-ax3.scatter(bin_center, dir_vario[1], label="1. axis")
-ax3.scatter(bin_center, dir_vario[2], label="2. axis")
-model.plot("vario_axis", axis=0, ax=ax3, label="fit on axis 0")
-model.plot("vario_axis", axis=1, ax=ax3, label="fit on axis 1")
-model.plot("vario_axis", axis=2, ax=ax3, label="fit on axis 2")
-ax3.set_title("Fitting an anisotropic model")
-ax3.legend()
+x_max = bin_center[-1]
+ax2.scatter(bin_center, dir_vario[0], label="0. axis")
+ax2.scatter(bin_center, dir_vario[1], label="1. axis")
+ax2.scatter(bin_center, dir_vario[2], label="2. axis")
+model.plot("vario_axis", axis=0, ax=ax2, x_max=x_max, label="fit on axis 0")
+model.plot("vario_axis", axis=1, ax=ax2, x_max=x_max, label="fit on axis 1")
+model.plot("vario_axis", axis=2, ax=ax2, x_max=x_max, label="fit on axis 2")
+ax2.set_title("Fitting an anisotropic model")
+ax2.legend()
 
 plt.show()
+
+###############################################################################
+# Also, let's have a look at the field.
+
+srf.plot()
