9eb36265209a4f9271b5a64b6447f4c21d27dd66
Sebastian Müller <mueller.seb@posteo.de>
2021-03-22 19:20:02 +0100

    Setup: remove py35 classifier

--- setup.py
+++ setup.py
@@ -216,7 +216,6 @@ CLASSIFIERS = [
     "Operating System :: Unix",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
