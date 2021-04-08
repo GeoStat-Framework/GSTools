17db6fd4f04f8992515faa3a9f876dea53d19a50
Sebastian Müller <mueller.seb@posteo.de>
2021-04-08 23:40:29 +0200

    pylint: handle pylint warnings in gstools.tools

--- gstools/tools/misc.py
+++ gstools/tools/misc.py
@@ -11,6 +11,8 @@ The following functions are provided
    list_format
    eval_func
 """
+# pylint: disable=C0103, C0415
+
 import numpy as np
 from gstools.tools.geometric import format_struct_pos_dim, generate_grid
 
