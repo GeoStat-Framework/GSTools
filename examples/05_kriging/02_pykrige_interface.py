b667e1776c1294c7eee648f698f286d21aaae4db
Sebastian Müller <mueller.seb@posteo.de>
2021-04-12 11:17:57 +0200

    PyKrige: update interface example again

--- examples/05_kriging/02_pykrige_interface.py
+++ examples/05_kriging/02_pykrige_interface.py
@@ -5,7 +5,7 @@ Interface to PyKrige
 To use fancier methods like
 `regression kriging <https://en.wikipedia.org/wiki/Regression-kriging>`__,
 we provide an interface to
-`PyKrige <https://github.com/GeoStat-Framework/PyKrige>`__, (>1.5) which means,
+`PyKrige <https://github.com/GeoStat-Framework/PyKrige>`__ (>v1.5), which means
 you can pass a GSTools covariance model to the kriging routines of PyKrige.
 
 To demonstrate the general workflow, we compare ordinary kriging of PyKrige
@@ -26,14 +26,17 @@ gridx = np.arange(0.0, 5.5, 0.1)
 gridy = np.arange(0.0, 6.5, 0.1)
 
 ###############################################################################
-# A GSTools based covariance model.
+# A GSTools based :any:`Gaussian` covariance model:
 
 model = gs.Gaussian(
     dim=2, len_scale=1, anis=0.2, angles=-0.5, var=0.5, nugget=0.1
 )
 
 ###############################################################################
-# Ordinary kriging with PyKrige. One can pass the defined GSTools model as
+# Ordinary Kriging with PyKrige
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# One can pass the defined GSTools model as
 # variogram model, which will `not` be fitted to the given data.
 # By providing the GSTools model, rotation and anisotropy are also
 # automatically defined:
@@ -44,8 +47,13 @@ plt.imshow(z1, origin="lower")
 plt.show()
 
 ###############################################################################
-# Ordinary kriging with gstools for comparison. PyKrige's routines are
-# using exact kriging by default (when a nugget is present).
+# Ordinary Kriging with GSTools
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# The :any:`Ordinary` kriging class is provided by GSTools as a shortcut to
+# define ordinary kriging with the general :any:`Krige` class.
+#
+# PyKrige's routines are using exact kriging by default (when given a nugget).
 # To reproduce this behavior in GSTools, we have to set ``exact=True``.
 
 OK2 = gs.krige.Ordinary(model, [cond_x, cond_y], cond_val, exact=True)
