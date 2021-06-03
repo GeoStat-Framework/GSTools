68e2c5391bc5c1040dc02c0c34304b2c42ac9f84
Sebastian Müller <mueller.seb@posteo.de>
2021-06-03 15:06:14 +0200

    Field: use single RandomState in example

--- examples/00_misc/05_standalone_field.py
+++ examples/00_misc/05_standalone_field.py
@@ -10,11 +10,12 @@ random values and plot them.
 import numpy as np
 import gstools as gs
 
-x1 = np.random.RandomState(19970221).rand(10000) * 100.0
-x2 = np.random.RandomState(20011012).rand(10000) * 100.0
-x3 = np.random.RandomState(20210530).rand(10000) * 100.0
-x4 = np.random.RandomState(20210531).rand(10000) * 100.0
-values = np.random.RandomState(2021).rand(10000) * 100.0
+rng = np.random.RandomState(19970221)
+x0 = rng.rand(10000) * 100.0
+x1 = rng.rand(10000) * 100.0
+x2 = rng.rand(10000) * 100.0
+x3 = rng.rand(10000) * 100.0
+values = rng.rand(10000) * 100.0
 
 ###############################################################################
 # Only thing needed to instantiate the Field is the dimension.
@@ -23,5 +24,5 @@ values = np.random.RandomState(2021).rand(10000) * 100.0
 # (:any:`SRF`, :any:`Krige` or :any:`CondSRF`), but with an additional field.
 
 plotter = gs.field.Field(dim=4)
-plotter(pos=(x1, x2, x3, x4), field=values)
+plotter(pos=(x0, x1, x2, x3), field=values)
 plotter.plot()
