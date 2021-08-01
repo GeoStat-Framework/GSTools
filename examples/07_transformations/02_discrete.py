0b7978b85216bc0783234c5197a6c3c1f739d2c7
Sebastian Müller <mueller.seb@posteo.de>
2021-08-01 18:37:00 +0200

    transform: update discrete example to store fields

--- examples/07_transformations/02_discrete.py
+++ examples/07_transformations/02_discrete.py
@@ -12,28 +12,32 @@ See :any:`transform.discrete`
 import numpy as np
 import gstools as gs
 
-# structured field with a size of 100x100 and a grid-size of 0.5x0.5
+# Structured field with a size of 100x100 and a grid-size of 0.5x0.5
 x = y = np.arange(200) * 0.5
 model = gs.Gaussian(dim=2, var=1, len_scale=5)
 srf = gs.SRF(model, seed=20170519)
-
-# create 5 equidistanly spaced values, thresholds are the arithmetic means
 srf.structured([x, y])
-discrete_values = np.linspace(np.min(srf.field), np.max(srf.field), 5)
-srf.transform("discrete", values=discrete_values)
-srf.plot()
 
+###############################################################################
+# Create 5 equidistanly spaced values, thresholds are the arithmetic means
+
+values1 = np.linspace(np.min(srf.field), np.max(srf.field), 5)
+srf.transform("discrete", store="f1", values=values1)
+srf.plot("f1")
+
+###############################################################################
 # calculate thresholds for equal shares
 # but apply different values to the separated classes
-discrete_values2 = [0, -1, 2, -3, 4]
-srf.structured([x, y])
-srf.transform("discrete", values=discrete_values2, thresholds="equal")
-srf.plot()
 
-# user defined thresholds
+values2 = [0, -1, 2, -3, 4]
+srf.transform("discrete", store="f2", values=values2, thresholds="equal")
+srf.plot("f2")
+
+###############################################################################
+# Create user defined thresholds
+# and apply different values to the separated classes
+
+values3 = [0, 1, 10]
 thresholds = [-1, 1]
-# apply different values to the separated classes
-discrete_values3 = [0, 1, 10]
-srf.structured([x, y])
-srf.transform("discrete", values=discrete_values3, thresholds=thresholds)
-srf.plot()
+srf.transform("discrete", store="f3", values=values3, thresholds=thresholds)
+srf.plot("f3")
