d6aba851c6e0e206cf36774d1ccff418ce25a9c6
Sebastian Müller <mueller.seb@posteo.de>
2021-08-02 19:53:15 +0200

    Examples: use storage control in 2D cond ensemble

--- examples/06_conditioned_fields/01_2D_condition_ensemble.py
+++ examples/06_conditioned_fields/01_2D_condition_ensemble.py
@@ -20,14 +20,14 @@ y = np.arange(0, 5, 0.1)
 model = gs.Gaussian(dim=2, var=0.5, len_scale=5, anis=0.5, angles=-0.5)
 krige = gs.Krige(model, cond_pos=cond_pos, cond_val=cond_val)
 cond_srf = gs.CondSRF(krige)
+cond_srf.set_pos([x, y], "structured")
 
 ###############################################################################
 # We create a list containing the generated conditioned fields.
 
 ens_no = 4
-field = []
 for i in range(ens_no):
-    field.append(cond_srf.structured([x, y], seed=i))
+    cond_srf(seed=i, store=f"f{i}")
 
 ###############################################################################
 # Now let's have a look at the pairwise differences between the generated
@@ -35,19 +35,22 @@ for i in range(ens_no):
 
 fig, ax = plt.subplots(ens_no + 1, ens_no + 1, figsize=(8, 8))
 # plotting kwargs for scatter and image
-sc_kwargs = dict(c=cond_val, edgecolors="k", vmin=0, vmax=np.max(field))
-im_kwargs = dict(extent=2 * [0, 5], origin="lower", vmin=0, vmax=np.max(field))
+vmax = np.max([cond_srf[f"f{i}"] for i in range(ens_no)])
+sc_kwargs = dict(c=cond_val, edgecolors="k", vmin=0, vmax=vmax)
+im_kwargs = dict(extent=2 * [0, 5], origin="lower", vmin=0, vmax=vmax)
 for i in range(ens_no):
     # conditioned fields and conditions
-    ax[i + 1, 0].imshow(field[i].T, **im_kwargs)
+    ax[i + 1, 0].imshow(cond_srf[f"f{i}"].T, **im_kwargs)
     ax[i + 1, 0].scatter(*cond_pos, **sc_kwargs)
     ax[i + 1, 0].set_ylabel(f"Field {i+1}", fontsize=10)
-    ax[0, i + 1].imshow(field[i].T, **im_kwargs)
+    ax[0, i + 1].imshow(cond_srf[f"f{i}"].T, **im_kwargs)
     ax[0, i + 1].scatter(*cond_pos, **sc_kwargs)
     ax[0, i + 1].set_title(f"Field {i+1}", fontsize=10)
     # absolute differences
     for j in range(ens_no):
-        ax[i + 1, j + 1].imshow(np.abs(field[i] - field[j]).T, **im_kwargs)
+        ax[i + 1, j + 1].imshow(
+            np.abs(cond_srf[f"f{i}"] - cond_srf[f"f{j}"]).T, **im_kwargs
+        )
 
 # beautify plots
 ax[0, 0].axis("off")
