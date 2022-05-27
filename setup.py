474cbd0b17d31a652d948dab62d4de42684426f1
Sebastian Müller <mueller.seb@posteo.de>
2022-05-28 00:05:45 +0200

    setup: cleanup

--- setup.py
+++ setup.py
@@ -13,21 +13,18 @@ CY_FILES = [
     {"name": "gstools.variogram.estimator", "language": "c++"},
     {"name": "gstools.krige.krigesum", "language": "c"},
 ]
-CY_MODULES = []
-for file in CY_FILES:
-    CY_MODULES.append(
-        Extension(
-            name=file["name"],
-            sources=[os.path.join("src", *file["name"].split(".")) + ".pyx"],
-            language=file["language"],
-            include_dirs=[np.get_include()],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-        )
+CY_MODULES = [
+    Extension(
+        name=file["name"],
+        sources=[os.path.join("src", *file["name"].split(".")) + ".pyx"],
+        language=file["language"],
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
-
+    for file in CY_FILES
+]
 # you can set GSTOOLS_BUILD_PARALLEL=0 or GSTOOLS_BUILD_PARALLEL=1
-USE_OPENMP = bool(int(os.getenv("GSTOOLS_BUILD_PARALLEL", "0")))
-if USE_OPENMP:
+if int(os.getenv("GSTOOLS_BUILD_PARALLEL", "0")):
     for mod in CY_MODULES:
         openmp_flags_added = add_openmp_flags_if_available(mod)
     if openmp_flags_added:
@@ -37,4 +34,5 @@ if USE_OPENMP:
 else:
     print("## GSTOOLS setup: OpenMP not wanted by the user.")
 
+# setup - do not include package data to ignore .pyx files in wheels
 setup(ext_modules=cythonize(CY_MODULES), include_package_data=False)
