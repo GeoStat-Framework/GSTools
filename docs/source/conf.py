fbc6e6b0a043cf8574c1a303fdc666b1edc62d84
Sebastian Müller <mueller.seb@posteo.de>
2022-10-19 15:14:27 +0200

    doc: keep 'Other Parameters' section; better align properties in html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,10 @@ autodoc_member_order = "groupwise"
 # Notes in boxes
 napoleon_use_admonition_for_notes = True
 # Attributes like parameters
-# napoleon_use_ivar = True
+napoleon_use_ivar = True
+# keep "Other Parameters" section
+# https://github.com/sphinx-doc/sphinx/issues/10330
+napoleon_use_param = False
 # this is a nice class-doc layout
 numpydoc_show_class_members = True
 # class members have no separate file, so they are not in a toctree
@@ -125,7 +128,7 @@ release = ver
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -164,7 +167,11 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ["custom.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
