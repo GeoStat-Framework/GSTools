1d59d9ca592d63bca5ed5c1627730a2899d173da
LSchueler <mostem@posteo.de>
2024-10-30 20:47:00 +0100

    Initial commit for plurigaussian fields

---
+++ src/gstools/field/pgs.py
@@ -0,0 +1,58 @@
+"""
+GStools subpackage providing plurigaussian simulation (PGS)
+
+.. currentmodule:: gstools.field.pgs
+
+The following classes are provided
+
+.. autosummary::
+   :toctree:
+
+   PGS
+"""
+
+import numpy as np
+
+
+class PGS:
+    def __init__(self, dim, fields, facies, mesh_type="unstructured"):
+        # TODO check that dimensions, domain size, ... are the same for all
+        # fields
+        self._dim = dim
+        self._Zs = fields
+        self._L = facies
+        self._P = self.calc_pgs()
+
+    def calc_pgs(self):
+        # TODO so far, this only works with struct fields
+        try:
+            mapping = np.stack(self._Zs, axis=1)
+        except np.exceptions.AxisError:
+            # if dim==1, `fields` is prob. a raw field & not a 1-tuple or
+            # equivalent
+            if self._dim == 1:
+                self._Zs = [self._Zs]
+                mapping = np.stack(self._Zs, axis=1)
+            else:
+                raise
+        pos_l = []
+        for d in range(self._dim):
+            pos_l.append(
+                np.linspace(
+                    np.floor(self._Zs[d].min()),
+                    np.ceil(self._Zs[d].max()),
+                    self._Zs[d].shape[d],
+                )
+            )
+
+        P_dig = []
+        for d in range(self._dim):
+            P_dig.append(np.digitize(mapping[:, d], pos_l[d]))
+
+        return self._L[*P_dig].reshape(self._Zs[0].shape)
+
+    @property
+    def P(self):
+        """:class:`str`: Plurigaussian field
+        """
+        return self._P
