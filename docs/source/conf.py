e83936b76ecc46852b4c6d807e0de50d2711fd41
Sebastian Müller <mueller.seb@posteo.de>
2025-01-29 15:23:22 +0100

    blackened

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -233,9 +233,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, "GSTools", "GSTools Documentation", [author], 1)
-]
+man_pages = [(master_doc, "GSTools", "GSTools Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
